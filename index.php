10453d859ef854d23be274c721ad29c97e021481
Ethan DeSon <edeson@ucsd.edu>
2015-06-18 17:39:52 -0700

    pointing webpages to new OpenShift databases

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 <?php
     session_start();
     require_once('config.php');
-        $database = "a7735960_text";  
-        $user = "a7735960_text";
         $db_handle = mysqli_connect( $host, $user, $pass, $database)
                         or die("Failed to connect");
         mysqli_select_db($db_handle, $database);
