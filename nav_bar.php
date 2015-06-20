19c3e2bfb1ad3967683fd08487ed78291a1e9bfb
Ethan DeSon <edeson@ucsd.edu>
2015-06-19 17:49:32 -0700

    inline-block elements are affected by whitespace in the html document where they are place, must comment out whitespace

--- nav_bar.php
+++ nav_bar.php
@@ -5,32 +5,11 @@
     <div class="nav fadeIn">
         <div class="container">
                 <ul>
-                    <li>
-                           <div class="btn fadeIn">
-                            <a href="index.php">HOME</a>
-                           </div> 
-		    </li><!-- white space affects rending of inline-blocks 
-			      so its necessary to comment out the whitespace
-                    --><li> 
-                       <div class="btn fadeIn">
-                            <a href="about.php">ABOUT</a>
-                       </div> 
-                     </li><!--
-                   --><li> 
-                       <div class="btn fadeIn">
-                            <a href="contact.php">CONTACT</a>
-                       </div> 
-                    </li><!--
-                   --><li> 
-                        <div class="btn fadeIn">
-                            <a href="login.php">LOGIN</a>
-                        </div> 
-                    </li><!--
-                     --><li> 
-                       <div class="btn fadeIn">
-                            <a href="register.php">REGISTER</a>
-                       </div> 
-                    </li>
+                    <li class="btn fadeIn"><a href="index.php">HOME</a></li><!--                        
+                 --><li class="btn fadeIn"><a href="about.php">ABOUT</a></li><!--
+                 --><li class="btn fadeIn"><a href="contact.php">CONTACT</a></li><!-- 			
+                 --><li class="btn fadeIn"><a href="login.php">LOGIN</a></li><!-- 
+                 --><li class="btn fadeIn"><a href="register.php">REGISTER</a></li> 
                 </ul>
              </div>
          </div>
