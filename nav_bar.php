65518a7f52dcbfb930ed64119b63270aea2cc9aa
Ethan DeSon <edeson@ucsd.edu>
2015-06-20 00:10:15 -0700

    tweeking nav bar sizing

--- nav_bar.php
+++ nav_bar.php
@@ -5,20 +5,14 @@
     <div class="nav fadeIn">
         <div class="container">
                 <ul>
-                    <li class="btn fadeIn"><a href="index.php"><span onload="sizeButton()">HOME</span></a></li><!--                        
-                 --><li class="btn fadeIn"><a href="about.php"><span onload="sizeButton()">ABOUT</span></a></li><!--
-                 --><li class="btn fadeIn"><a href="contact.php"><span onload="sizeButton()">CONTACT</span></a></li><!-- 			
-                 --><li class="btn fadeIn"><a href="login.php"><span onload="sizeButton()">LOGIN</span></a></li><!-- 
-                 --><li class="btn fadeIn"><a href="register.php"><span onload="sizeButton()">REGISTER</span></a></li> 
+                    <li class="btn fadeIn"><a href="index.php"><span>HOME</span></a></li><!--                        
+                 --><li class="btn fadeIn"><a href="about.php"><span>ABOUT</span></a></li><!--
+                 --><li class="btn fadeIn"><a href="contact.php"><span>CONTACT</span></a></li><!-- 			
+                 --><li class="btn fadeIn"><a href="login.php"><span>LOGIN</span></a></li><!-- 
+                 --><li class="btn fadeIn"><a href="register.php"><span>REGISTER</span></a></li> 
                 </ul>
              </div>
          </div>
     </td>
    </tr>
 </tbody></table>
-<script>
-function sizeButton() {
-        var currSize = document.getElementByClass("btn");
-        document.getElementByTagName("span").style.fontSize = currSize;
-}
-</script>
